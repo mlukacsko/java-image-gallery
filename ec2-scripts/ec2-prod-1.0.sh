d9d76d67b4139c293ee64e6ba4336337f9f35629
EC2 Default User <ec2-user@ip-10-0-1-242.us-east-2.compute.internal>
2020-06-29 00:14:26 +0000

    prod boot script update

--- ec2-scripts/ec2-prod-1.0.sh
+++ ec2-scripts/ec2-prod-1.0.sh
@@ -7,11 +7,25 @@ amazon-linux-extras install -y java-openjdk11
 amazon-linux-extras install -y nginx1
 yum install -y java-11-openjdk-devel git
 su ec2-user -l -c 'curl -s "https://get.sdkman.io" | bash && source .bashrc && sdk install gradle'
+sudo yum install -y postgresql
+sudo yum install -y gcc
+sudo yum install -y python3-devel
+sudo yum install -y postgresql-devel
+sudo yum install -y jq # json parser
+sudo pip3 install psycopg2
+sudo pip3 install boto3
+cd /home/ec2-user
+pip3 install boto3 --user
+pip3 install psycopg2 --user
+
+
 
 # Configure/install custom software
 cd /home/ec2-user
 git clone https://github.com/cdavidshaffer/java-image-gallery.git
+git clone https://github.com/mlukacsko/ansible.git
 chown -R ec2-user:ec2-user java-image-gallery
+chown -R ec2-user:ec2-user ansible
 
 CONFIG_BUCKET="s3://edu.au.cc.image-gallery-config"
 aws s3 cp ${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx/nginx.conf
